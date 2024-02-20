db9e4c9e7ca6180f855c3429508c1f1dfbe003d4
longtran <tranlong204@gmail.com>
2024-02-20 13:34:13 -0800

    Add props for parent and child components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import ReactDOM from "react-dom";
+import Parent from "./props/Parent";
 
 const App = () => {
   return (
-    <div>
-      <h1> Hi there!</h1>
-    </div>
+    <>
+      <div>
+        <h1> Hi there!</h1>
+      </div>
+
+      <div>
+        <Parent />
+      </div>
+    </>
   );
 };
 
